db861ee4437e9fbb5fc0ac5ee839cfbf247da19e
kwb020312 <kwb020312@naver.com>
2019-06-04 11:29:55 +0900

    empENAME

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : '111111',
+  database        : 'kimwoobin'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  pool.getConnection(function(err, conn){
+    conn.query('SELECT * FROM emp;',function(err, results){
+      console.log(`------`);
+      console.log(results);
+
+      res.render('index', { results : results});
+
+      conn.release();
+    });
+  });
 });
 
 module.exports = router;
